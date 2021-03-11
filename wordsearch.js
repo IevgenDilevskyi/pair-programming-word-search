5468852bd4eeb93245bc871953232e407084e1e8
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-11 03:23:10 +0000

    implemented wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
+// Pair Programming w/ Ievgen, Violet, Caden
+
+const transpose = function(matrix) {
+    let newMatrix = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      newMatrix.push([]);
+    }
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        newMatrix[j].push(matrix[i][j]);
+      }
+    }
+    return newMatrix;
+  };
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    // console.log(horizontalJoin);
+    for (let l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    // console.log(verticalJoin);
+    for (let c of verticalJoin) {
+        if (c.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
